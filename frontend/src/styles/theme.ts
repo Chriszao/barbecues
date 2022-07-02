aa8d08d9f574ad1e663442edc731848de86705a8
Chriszao <chris.f.assis18@gmail.com>
2022-07-02 13:08:49 -0300

    feat: Add theme config

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,33 @@
+export default {
+  border: {
+    radius: '0.125rem',
+  },
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    medium: 500,
+    bold: 700,
+    extraBold: 800,
+    sizes: {
+      small: '1.125rem',
+      medium: '1.25rem',
+      large: '1.75rem',
+      xLarge: '2rem',
+      xxLarge: '2.25rem',
+    },
+  },
+  colors: {
+    primary: '#FFD836',
+    primaryLightened: '#ffe472',
+    primaryDimmed: '#998220',
+    mainBg: '#FAFAFA',
+    white: '#FFFFFF',
+    black: '#000000',
+    lightGray: '#F1F1F1',
+  },
+  spacings: {
+    medium: '1.5rem',
+    large: '3rem',
+    xLarge: '4.375rem',
+  },
+};
