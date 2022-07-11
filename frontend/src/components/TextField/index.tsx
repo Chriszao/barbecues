faf9cfb9bf07a8468064c958ed6b314b2abb106c
Chriszao <chris.f.assis18@gmail.com>
2022-07-10 21:04:10 -0300

    feat: Add TextField component and styles

--- frontend/src/components/TextField/index.tsx
+++ frontend/src/components/TextField/index.tsx
@@ -1,39 +1,115 @@
-import type { InputHTMLAttributes, ReactElement } from 'react';
+import type {
+  InputHTMLAttributes,
+  ReactElement,
+  TextareaHTMLAttributes,
+} from 'react';
+import type { RegisterOptions } from 'react-hook-form';
 import { useController, useFormContext } from 'react-hook-form';
+import type { NumberFormatProps } from 'react-number-format';
+import NumberFormat from 'react-number-format';
 
 import * as S from './styles';
 
+type MaskTypes = 'money' | 'number';
+
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  rules?: unknown;
+  rules?: RegisterOptions;
   name: string;
+  defaultValue?: string | number | readonly string[];
+  label?: string;
+  maskType?: MaskTypes;
+  useTextArea?: boolean;
+  fullWidth?: boolean;
 }
 
 export function TextField({
+  id,
   name,
   placeholder,
   required,
+  defaultValue,
+  label,
+  maskType,
+  useTextArea,
+  fullWidth,
   ...inputProperties
 }: InputProps): ReactElement {
   const { control } = useFormContext();
 
-  const baseRules = {
-    required: { message: 'Campo obrigatório', value: !!required },
-  };
+  let baseRules = {};
+
+  if (required) {
+    baseRules = {
+      required: { message: 'Campo obrigatório' },
+    };
+  }
 
   const { field, fieldState } = useController({
     name,
     control,
     rules: { ...baseRules },
+    defaultValue,
   });
 
+  const maskTypes = {
+    number: {
+      thousandSeparator: '.',
+      decimalSeparator: ',',
+      placeholder: '00.00',
+    },
+    money: {
+      thousandSeparator: '.',
+      decimalSeparator: ',',
+      placeholder: '0,00',
+      decimalScale: 2,
+      fixedDecimalScale: true,
+      isAllowed: ({ value: valueString }: { value: string }) =>
+        valueString.length < 18,
+    },
+    date: { format: '##/##/####', placeholder: '__/__/____' },
+  };
+
   return (
-    <S.Wrapper>
-      <input
-        {...field}
-        {...inputProperties}
-        name={name}
-        placeholder={placeholder}
-      />
+    <S.Wrapper
+      role="textbox"
+      haveMask={!!maskType}
+      fullWidth={fullWidth}
+      isWithError={!!fieldState.error?.message}
+    >
+      {label && <S.Label htmlFor={name}>{label}</S.Label>}
+
+      {maskType && (
+        <>
+          <S.Adornment>R$</S.Adornment>
+          <NumberFormat
+            {...(inputProperties as NumberFormatProps)}
+            {...maskTypes[maskType]}
+            {...field}
+            name={name}
+            id={id}
+          ></NumberFormat>
+        </>
+      )}
+      {!maskType && useTextArea && (
+        <textarea
+          {...(inputProperties as TextareaHTMLAttributes<HTMLTextAreaElement>)}
+          {...field}
+          name={name}
+          id={id}
+        ></textarea>
+      )}
+
+      {!maskType && !useTextArea && (
+        <input
+          {...field}
+          {...inputProperties}
+          id={id}
+          name={name}
+          placeholder={placeholder}
+        />
+      )}
+
+      <S.ErrorHelperText>{fieldState.error?.message}</S.ErrorHelperText>
     </S.Wrapper>
   );
 }
--- frontend/src/components/TextField/styles.ts
+++ frontend/src/components/TextField/styles.ts
@@ -1,22 +1,92 @@
 import styled, { css } from 'styled-components';
 
-export const Wrapper = styled.div`
-  ${({ theme }) => css`
+interface WrapperProps {
+  haveMask?: boolean;
+  fullWidth?: boolean;
+  isWithError?: boolean;
+}
+
+export const Wrapper = styled.div<WrapperProps>`
+  ${({ theme, haveMask, fullWidth, isWithError }) => css`
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     justify-content: center;
     margin: 10px 0;
+    position: relative;
+    border-radius: 10px;
+    ${fullWidth &&
+    css`
+      width: 100%;
+    `}
 
     & > input {
+      width: 100%;
+      border: none;
+      border-radius: 10px;
+      padding: ${haveMask ? '16px 16px 16px 48px' : '16px'};
+      background: ${theme.colors.lightGray};
+      font-size: ${theme.font.sizes.small};
+      outline: none;
+      transition: all 0.2s ease-in-out;
+
+      &::placeholder {
+        ${haveMask &&
+        css`
+          color: ${theme.colors.black};
+        `}
+      }
+
+      &:focus {
+        box-shadow: 0px 10px 40px #00000056;
+      }
+
+      ${isWithError &&
+      css`
+        border: 1px solid red;
+
+        &::placeholder {
+          color: ${theme.colors.error};
+        }
+      `}
+    }
+
+    & > textarea {
       width: 100%;
       border: none;
       border-radius: 10px;
       padding: 16px;
       background: ${theme.colors.lightGray};
       font-size: ${theme.font.sizes.small};
-      box-shadow: 0px 10px 40px #00000056;
+      font-family: ${theme.font.family};
       outline: none;
+      transition: all 0.2s ease-in-out;
+
+      &:focus {
+        box-shadow: 0px 10px 40px #00000056;
+      }
     }
+  `}
+`;
+
+export const ErrorHelperText = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    padding-left: 8px;
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.error};
+  `}
+`;
+
+export const Adornment = styled.div`
+  position: absolute;
+  left: 1.125rem;
+  top: 2.563rem;
+`;
+
+export const Label = styled.label`
+  ${({ theme }) => css`
+    padding-left: 10px;
+    font-size: ${theme.font.sizes.small};
   `}
 `;
