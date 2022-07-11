b9397c01731caaf628e98f2d797ab1c0c649ea8e
Chriszao <chris.f.assis18@gmail.com>
2022-07-10 21:07:36 -0300

    feat: Configure query client, authprovider and toast container in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,38 @@
-import { Router } from 'components/Routes';
-import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from 'hooks/useAuth';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ToastContainer } from 'react-toastify';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from 'styles/global';
 import theme from 'styles/theme';
 
+import { Router } from './Routes/Router';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 5,
+      },
+    },
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <ToastContainer
+        position="bottom-left"
+        closeOnClick
+        style={{
+          fontFamily: theme.font.family,
+          fontSize: theme.font.sizes.small,
+        }}
+      />
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <Router />
+        </AuthProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
